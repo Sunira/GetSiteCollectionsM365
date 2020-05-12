419913fb697784b351a8f3d2669848b293661d07
Sunira Moses <sunira.moses@gmail.com>
2020-05-11 20:55:05 -0400

    Implemented Saving to .CSV

--- GetSiteCollectionM365/Program.cs
+++ GetSiteCollectionM365/Program.cs
@@ -1,24 +1,35 @@
 ﻿
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Windows.Forms;
+
 
 using Microsoft.SharePoint.Client;
 using Microsoft.Online.SharePoint.TenantAdministration;
 
 using OfficeDevPnP.Core.Entities;
-
+using CsvHelper;
+using AngleSharp.Dom.Css;
 
 namespace GetSiteCollectionsM365
 {
+    //Defined for use of CSV Helper
+    public class M365SiteCollection
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public DateTime LastModified { get; set; }
+    }
+
     class GetSiteCollections
     {
         [STAThread]
         static void Main(string[] args)
         {
             //https://suniradev-admin.sharepoint.com/ - Test SharePoint Tenant URL
-
+            
             Console.WriteLine("Attempting to Log into M365! ");
             LoginUsingM365();
         }
@@ -48,32 +59,64 @@ namespace GetSiteCollectionsM365
         {
             /*Get Site Collections under Tenant, including OneDrive Sites*/
             Tenant tenant = new Tenant(context);
-            IList<SiteEntity> siteCols = tenant.GetSiteCollections(startIndex: 0,
-                                                     includeDetail: true,
-                                                     includeOD4BSites: true);
+            IList<SiteEntity> siteCols = tenant.GetSiteCollections(startIndex: 0, includeDetail: true, includeOD4BSites: true);
 
-            foreach(var siteCol in siteCols)
+            var records = new List<M365SiteCollection>();
+
+            foreach (var siteCol in siteCols)
             {
-                string title;
+                string siteTitle = siteCol.Title;
+                string siteUrl = siteCol.Url.ToString();
+                DateTime lastMod = siteCol.LastContentModifiedDate;
 
-                title = siteCol.Url.ToString().Contains("-my.sharepoint.com/") ? "Onedrive Site" :
-                        siteCol.Url.ToString().Length < 1 ? "No Title" : 
-                        siteCol.Title; 
+                siteTitle = siteUrl.Contains("-my.sharepoint.com") ? "Onedrive for Business" :
+                    siteUrl.Contains(".sharepoint.com/search") ? "Search" :
+                            siteUrl.Length < 1 ? "No Title" :
+                            siteTitle; 
                 
-                Console.WriteLine(title + " : " + siteCol.Url.ToString() + " Modified: " + siteCol.LastContentModifiedDate);
+                Console.WriteLine(siteTitle + " : " + siteUrl + " Modified: " + lastMod.ToString());
+                records.Add(new M365SiteCollection { 
+                    Name = siteTitle, 
+                    Url = siteCol.Url.ToString(), 
+                    LastModified = lastMod 
+                });
             }
 
-            SaveToCSV();
+            SaveToCSV(records);
         }
 
-        private static void SaveToCSV()
+        private static void SaveToCSV(List<M365SiteCollection> records)
         {
+            string fileName = "site-collections";
+            string filePath = "/";
+            Console.WriteLine("Please Select the Folder to save the CSV file. Press Enter to Continue.");
+            while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter))
+            {
+                //Just Hang Out until I get a keypress.
+            }
+
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             if (fbd.ShowDialog() == DialogResult.OK)
             {
-                Console.WriteLine(fbd.SelectedPath); // full path
+                filePath = fbd.SelectedPath;
             }
 
+            do
+            {
+                Console.WriteLine("Please give the .csv file a name, without the extension.");
+                fileName = Console.ReadLine();
+            } while (fileName.Length < 1);
+
+            string fileRoute = filePath + "/" + fileName + ".csv";
+            Console.WriteLine("Saving to :" + fileRoute);
+
+            using (var writer = new StreamWriter(fileRoute))
+            {
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(records);
+                }
+            }
         }
 
         private static bool ValidateTenantURL(string siteURL)
